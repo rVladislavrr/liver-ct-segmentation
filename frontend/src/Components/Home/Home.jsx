c7cadf5a035ccdf7633dd27a0b98004997ba0f2c
ibolonkin <ivanbolonkin111@gmail.com>
2025-03-21 16:03:12 +0500

    init commit frontend

---
+++ frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Home.css'
+const Home = () => {
+  return (
+    <>
+      <div className="header">
+        <div>
+          <ul className="header-list">
+            <li className="header-list-item">Профиль</li>
+            <li className="header-list-item">Q&A</li>
+            <li className="header-list-item">Выйти</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
