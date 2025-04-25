3d77211777efe98fe6ce194a34eb27948be90958
rVladislavrr <rvladislavrr@mail.ru>
2025-04-25 21:03:42 +0500

    fix

--- backend/src/api/v1/routers/photos.py
+++ backend/src/api/v1/routers/photos.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Request, BackgroundTasks, Depends, HTTPException, status, Response
+from fastapi import APIRouter, Request, BackgroundTasks, Depends, HTTPException, status, Response, Path
 from pydantic import UUID4
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.db.db import get_async_session
 from src.db.manager_files import fileManager
 from src.db.manager_photos import photos_manager
+from src.logger import api_logger, database_logger
 from src.schemas.photos import PhotoCreate
 from src.schemas.users import UserPhoto
 from src.service.model import modelManager
@@ -59,46 +60,89 @@ async def delete_photo(photo_id: UUID4,
                        request: Request,
                        session: AsyncSession = Depends(get_async_session)):
     request_id = request.state.request_id
-    user_id = request.state.user_id
-    photo_delete = await photos_manager.delete_photo(session, photo_id, user_id, request_id)
-    background_task.add_task(photos_manager.delete_photos_all, photo_id, user_id, request_id)
+    try:
+        request_id = request.state.request_id
+        user_id = request.state.user_id
+        photo_delete = await photos_manager.delete_photo(session, photo_id, user_id, request_id)
+        background_task.add_task(photos_manager.delete_photos_all, photo_id, user_id, request_id)
 
-    return photo_delete
+        return photo_delete
+    except HTTPException:
+        raise
+    except Exception as e:
+        database_logger.error(
+            "Failed delete photo ",
+            extra={
+                "photo_id": photo_id,
+                "request_id": request_id,
+            },
+            exc_info=e,
+        )
 
 
 @router.get('/{file_uuid}/{num_slices}/photos')
-async def get_file_photo(background_task: BackgroundTasks, file_uuid: str, num_slices: int, request: Request,
+async def get_file_photo(background_task: BackgroundTasks, file_uuid: UUID4, request: Request,
+                         num_slices: int = Path(ge=0),
                          session=Depends(get_async_session)):
-
     request_id = request.state.request_id
-    if img := await get_clear_photo_cached(file_uuid, num_slices):
+    try:
+        if img := await get_clear_photo_cached(file_uuid, num_slices):
+            return Response(content=img, media_type="image/png")
+
+        file_bytes = await get_file_bytes(background_task, file_uuid, session=session, request_id=request_id,
+                                          num_images=num_slices)
+        image = modelManager.pred_image(file_bytes, num_slices)
+        img = modelManager.get_photo(image)
+        background_task.add_task(load_clear_photo, file_uuid, num_slices, img)
+
         return Response(content=img, media_type="image/png")
+    except HTTPException:
+        raise
+    except Exception as e:
 
-    file_bytes = await get_file_bytes(background_task, file_uuid, session=session, request_id=request_id,
-                                      num_images=num_slices)
-    image = modelManager.pred_image(file_bytes, num_slices)
-    img = modelManager.get_photo(image)
-    background_task.add_task(load_clear_photo, file_uuid, num_slices, img)
-
-    return Response(content=img, media_type="image/png")
+        database_logger.error(
+            "Failed delete photo ",
+            extra={
+                "file_uuid": file_uuid,
+                'num_slices':num_slices,
+                "request_id": request_id,
+            },
+            exc_info=e,
+        )
 
 
 @router.get('/{file_uuid}/{num_slices}/contours')
-async def get_file_photo(background_task: BackgroundTasks, file_uuid: str, num_slices: int, request: Request,
+async def get_file_photo(background_task: BackgroundTasks, file_uuid: UUID4, request: Request,
+                         num_slices: int = Path(ge=0),
                          session=Depends(get_async_session)):
+    request_id = request.state.request_id
     try:
         if con := await get_contours_cached(file_uuid, num_slices):
             return con
         else:
-            request_id = request.state.request_id
+
             file_bytes = await get_file_bytes(background_task, file_uuid, session=session, request_id=request_id,
                                               num_images=num_slices)
             image = modelManager.pred_image(file_bytes, num_slices)
             con = modelManager.get_result_contours(image)
             background_task.add_task(load_contours_cached, file_uuid, num_slices, con)
             return con
-    except HTTPException:
+    except HTTPException as e:
+        api_logger.warning(
+            "Failed get_file_photo ",
+            extra={
+                "file_uuid": file_uuid,
+                "request_id": request_id,
+            },
+        )
         raise
     except Exception as e:
-        print(e)
-        raise HTTPException(500)
+        api_logger.error(
+            "Failed get_file_photo ",
+            extra={
+                "file_uuid": file_uuid,
+                "request_id": request_id,
+            },
+            exc_info=e,
+        )
+        raise HTTPException(500, detail={'msg': "Failed get_file_photo"})
